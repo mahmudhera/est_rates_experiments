2772ed455926a9de6a38c34a6ddfc213e2d58b82
Mahmudur <mahmudhera93@gmail.com>
2025-05-05 23:37:59 -0400

    updated for random.fasta

--- run_everything.sh
+++ run_everything.sh
@@ -15,15 +15,18 @@ python src/plot_results_varying_fAs.py --estimated_rates_file results/estimated_
 # run simulations
 python src/simulate_mutations.py data/ndl.fasta data/
 python src/simulate_mutations.py data/staphylococcus.fasta data/
+python src/simulate_mutations.py data/random.fasta data/
 
 # compute rates
 python estimate_rates.py data/ndl.fasta data/ 128 results/est_rates_ndl.csv
 python estimate_rates.py data/staphylococcus.fasta data/ 128 results/est_rates_staph.csv
 python estimate_rates.py data/ndl.fasta data/ 128 results/est_rates_ndl_using_known.csv --use_true_values
 python estimate_rates.py data/staphylococcus.fasta data/ 128 results/est_rates_staph_using_known.csv --use_true_values
+python estimate_rates.py data/random.fasta data/ 128 results/est_rates_random_using_known.csv --use_true_values
 
 # plot
 python src/plot_estimated_rates.py --estimated_rates_file results/est_rates_ndl.csv --output_filename plots/ndl_estimated_rates.pdf --method linear
 python src/plot_estimated_rates.py --estimated_rates_file results/est_rates_staph.csv --output_filename plots/staph_estimated_rates.pdf --method linear
 python src/plot_estimated_rates.py --estimated_rates_file results/est_rates_ndl_using_known.csv --output_filename plots/ndl_estimated_rates_using_known.pdf --method linear
 python src/plot_estimated_rates.py --estimated_rates_file results/est_rates_staph_using_known.csv --output_filename plots/staph_estimated_rates_using_known.pdf --method linear
+python src/plot_estimated_rates.py --estimated_rates_file results/est_rates_random_using_known.csv --output_filename plots/random_estimated_rates_using_known.pdf --method linear
